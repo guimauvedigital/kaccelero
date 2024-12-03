da4cf44280fdb5e3e90636570ad6a7f845ac5c3c
NathanFallet <contact@nathanfallet.me>
2024-12-03 10:38:41 +0100

    fix: js target

--- core/src/commonMain/kotlin/dev/kaccelero/commons/analytics/ILogEventUseCase.kt
+++ core/src/commonMain/kotlin/dev/kaccelero/commons/analytics/ILogEventUseCase.kt
@@ -1,9 +1,7 @@
 package dev.kaccelero.commons.analytics
 
 import dev.kaccelero.usecases.IPairUseCase
-import kotlin.js.JsExport
 
-@JsExport
 interface ILogEventUseCase :
     IPairUseCase<IAnalyticsEventName, Map<IAnalyticsEventParameter, IAnalyticsEventValue>, Unit> {
 
