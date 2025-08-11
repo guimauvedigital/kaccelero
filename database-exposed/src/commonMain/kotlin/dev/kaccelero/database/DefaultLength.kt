8d1141def1c77c36139a189c50df4d3f42172075
NathanFallet <contact@nathanfallet.me>
2025-08-11 12:53:44 +0200

    feat: add default lengths constants

---
+++ database-exposed/src/commonMain/kotlin/dev/kaccelero/database/DefaultLength.kt
@@ -0,0 +1,13 @@
+package dev.kaccelero.database
+
+/**
+ * Default lengths for various database fields.
+ */
+object DefaultLength {
+
+    /**
+     * Default length for VARCHAR fields in the database.
+     */
+    const val VARCHAR = 255
+
+}
